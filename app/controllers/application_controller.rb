b3a05e139cc1ad9cf8b46d23c8c7e10c1e1230c7
IgorShayderov <flashmob123@gmail.com>
2021-05-28 23:18:49 +0200

    Force user to authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  helper_method :current_user
+
+  private
+
+  def authenticate_user!
+    unless current_user
+      redirect_to sign_in_path
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def signed_in?
+    current_user.present?
+  end
 end
